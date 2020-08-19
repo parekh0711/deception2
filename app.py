5775d80667b2ca360307727c2c0c87bd6ccc3a0f
Soham Parekh <54930568+parekh0711@users.noreply.github.com>
2020-08-19 23:31:27 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
+import os
+import psycopg2
+
 
 @app.route('/',methods=['POST'])
 def myendpoint():
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     incoming_msg = request.values.get('Body', '').lower()
-    print(incoming_msg,file=open("output.txt","a+"))
+    cur.execute("""INSERT INTO votes VALUES('%s')""",incoming_msg)
 
 if __name__ == '__main__':
     app.run()
